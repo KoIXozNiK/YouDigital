8cf3c6caacc1e084e60b35dedd6125ac69f6236e
KoIXozNiK <vasylyk29@gmail.com>
2019-04-09 22:40:25 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,7 @@
     let menuH = $('.menu').outerHeight() || 0,
         windowH = $(window).outerHeight();
 
+
     $('.full-height-window').css('min-height', (windowH - menuH) + 'px');
 
     $('.full-height-window-hard').css('height', (windowH - menuH) + 'px');
@@ -22,14 +23,14 @@
   function mobileMenu() {
     if($(window).width() < 1025){
       let mobMenuW = $('.menu__right').outerWidth() || 0;
-      $('.menu__right').css('right', -mobMenuW );
+      $('.menu__right').css('transform', 'translateX('+ mobMenuW +'px)');
 
       $('.menu__hamburger i').on('click', function () {
-        $('.menu__right').css('right', 0).toggleClass('active')
+        $('.menu__right').css('transform', 'translateX('+0+')').toggleClass('active')
       })
 
       $('.menu__close').on('click', function () {
-        $('.menu__right').css('right', -mobMenuW ).toggleClass('active');
+        $('.menu__right').css('transform', 'translateX('+ mobMenuW +'px)').toggleClass('active');
       })
     }
   }
