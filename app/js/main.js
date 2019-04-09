70dd62847ed1ae35bdf9cd38787ef86347a5754f
KoIXozNiK <vasylyk29@gmail.com>
2019-04-09 19:32:45 +0300

    header

---
+++ app/js/main.js
@@ -0,0 +1,76 @@
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  /***********************/
+  /* FULL HEIGHT SECTION */
+  /***********************/
+
+  function fullHeightSection() {
+    let menuH = $('.menu').outerHeight() || 0,
+        windowH = $(window).outerHeight();
+
+    $('.full-height-window').css('min-height', (windowH - menuH) + 'px');
+
+    $('.full-height-window-hard').css('height', (windowH - menuH) + 'px');
+  }
+
+
+  /***************/
+  /* MOBILE MENU */
+  /***************/
+
+  function mobileMenu() {
+    if($(window).width() < 1025){
+      let mobMenuW = $('.menu__right').outerWidth() || 0;
+      $('.menu__right').css('right', -mobMenuW );
+
+      $('.menu__hamburger i').on('click', function () {
+        $('.menu__right').css('right', 0).toggleClass('active')
+      })
+
+      $('.menu__close').on('click', function () {
+        $('.menu__right').css('right', -mobMenuW ).toggleClass('active');
+      })
+    }
+  }
+
+
+  /***************/
+  /* MOBILE MENU */
+  /***************/
+
+  function changeLanguage(){
+    if($('.menu__language-wrap').length){
+      $('.menu__language-item').on('click', function () {
+        $('.menu__language-item').removeClass('active');
+        $(this).addClass('active')
+      })
+    }
+  }
+
+  /**********************************/
+  /* FUNCTION INITIALIZATION */
+  /**********************************/
+  $(window).on('load', function () {
+    fullHeightSection(),
+    mobileMenu()
+    changeLanguage()
+  });
+
+  $(window).on('load resize', function () {
+    fullHeightSection()
+  });
+
+  $(window).on('resize', function () {
+    mobileMenu()
+  });
+
+  $(window).on('scroll', function () {
+
+  });
+
+  window.addEventListener("orientationchange", function () {
+
+  });
+
+})(jQuery, window, document);
